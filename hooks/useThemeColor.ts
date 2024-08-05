a69aa0a2c7f5613b468dcfb2f398fa97c5cc1c93
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-08-05 18:51:12 +0100

    Initial commit
    Generated by create-expo-app 3.0.0.

---
+++ hooks/useThemeColor.ts
@@ -0,0 +1,17 @@
+/**
+ * Learn more about light and dark modes:
+ * https://docs.expo.dev/guides/color-schemes/
+ */
+
+import { useColorScheme } from 'react-native';
+import { Colors } from '@/constants/colors';
+
+export function useThemeColor(
+	props: { light?: string; dark?: string },
+	colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+) {
+	const theme = useColorScheme() ?? 'light';
+	const colorFromProps = props[theme];
+
+	return colorFromProps ?? Colors[theme][colorName];
+}
